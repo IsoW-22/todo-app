63d585bdbb8c78c90a9ff98855390f7001354a4e
Mostafa Zeighami <105848369+IsoW-22@users.noreply.github.com>
2022-08-16 17:27:23 +0430

    list full working

--- functions.js
+++ functions.js
@@ -1,11 +1,23 @@
+"use strict"
+
 const createNewTask = () => {
-    const itemCount = document.querySelector(".items").childNodes.length;
-    const main = document.querySelector(".items");
-    const create = document.getElementById("create");
+    const ses = sessionStorage.getItem("current div");
+    const itemCount = document.querySelector(`#${ses} .items`).childNodes.length;
+    const main = document.querySelector(`#${ses} .items`);
+    const create = document.querySelector(`#${ses} #create`);
+    const textarea = document.createElement("p");
+
+    let textInput = prompt("Please enter your task name:", "new task");
+    if (textInput == null || textInput == "") {
+      return;
+    } else {
+      textarea.innerHTML = textInput;
+    }
+    textarea.classList.add("task");
 
     const div = document.createElement("div");
     div.classList.add("todo-item");
-    div.setAttribute("id",`item-${itemCount - 3}`)
+    div.setAttribute("id",`item-${itemCount - 1}`)
     main.insertBefore(div, create);
 
     const tickButton = document.createElement("Button");
@@ -39,42 +51,13 @@ const createNewTask = () => {
     starButton.appendChild(star);
     div.appendChild(starButton);
 
-    const textarea = document.createElement("p");
-    textarea.classList.add("task");
-    
-    let textInput = prompt("Please enter your task name:", "new task");
-    if (textInput == null || textInput == "") {
-      textarea.innerHTML = "new task";
-    } else {
-      textarea.innerHTML = textInput;
-    }
     div.appendChild(textarea);
 }
 
-
-const create = document.getElementById("create");
+const create = document.querySelector(".create");
 create.addEventListener("click", createNewTask)
 
 // ^^^^^^^^^^^^^ up there... to make new tasks no? :D ^^^^^^^^^^
-
-function load(){
-    sessionStorage.clear();
-    sessionStorage.setItem("main", "[]");
-    if(localStorage.getItem("name") == null){
-      const name = prompt("enter your name", "new user");
-      if(name != null || name == ""){
-        localStorage.setItem("name", name);
-        document.querySelector(".user").innerHTML = name;
-      }
-      else {
-        document.querySelector(".user").innerHTML = "new user";
-      }
-    } else {
-        const newName = localStorage.getItem("name");
-        document.querySelector(".user").innerHTML = newName;
-    }
-    return;
-}
 
 const userChange = document.querySelector(".user");
 userChange.addEventListener("click", () => {
@@ -92,80 +75,104 @@ userChange.addEventListener("click", () => {
 // now lets do something more fun down here!
 //lets make these tasks buttons work :D so tick will... check it green? important yellow? ok
 
+
+function allButtons(event) {
+  const { target } = event;
+
+  if (target.classList.contains("tick-icon")) {
+    const task = target.parentNode.querySelector(".task");
+
+    if (target.parentNode.style.backgroundColor == "green") {
+      task.style.textDecoration = "none";
+      target.parentNode.style.backgroundColor = "#7A4495";
+      target.parentNode.querySelector(".edit-icon").disabled = false;
+      target.parentNode.querySelector(".important-button").disabled = false;
+      target.parentNode.querySelector(".important-button p").style.color =
+        "white";
+      target.parentNode.querySelector(".task").style.color = "#fff";
+    } else {
+      task.style.textDecoration = "line-through";
+      target.parentNode.style.backgroundColor = "green";
+      target.parentNode.querySelector(".edit-icon").disabled = true;
+      target.parentNode.querySelector(".important-button").disabled = true;
+      target.parentNode.querySelector(".task").style.color = "#fff";
+      const currentDivision = sessionStorage.getItem("current div");
+      let sesItem = JSON.parse(sessionStorage.getItem(`${currentDivision}-items`));
+      const id = target.parentNode.getAttribute("id");
+      const index = sesItem.indexOf(id);
+      if (index > -1) {
+        sesItem.splice(index, 1);
+      }
+      sessionStorage.removeItem(`${currentDivision}-items`);
+      sessionStorage.setItem(
+        `${currentDivision}-items`,
+        JSON.stringify(sesItem)
+      );
+    }
+  }
+
+  if (target.classList.contains("delete-icon")) {
+    if (confirm("are you sure you want to delete this task?")) {
+      target.parentNode.remove();
+    } else {
+      return;
+    }
+  }
+
+  if (target.classList.contains("edit-icon")) {
+    const pText = target.parentNode.querySelector(".task").innerHTML;
+    let textInput = prompt("Please enter your task name:", pText);
+    if (textInput == null || textInput == "") {
+      return;
+    } else {
+      target.parentNode.querySelector(".task").innerHTML = textInput;
+    }
+  }
+
+  if (target.classList.contains("important-button")) {
+    const id = target.parentNode.id;
+    const color = target.parentNode.querySelector(".important-button p").style
+      .color;
+      const currentDiv = sessionStorage.getItem("current div");
+    if (color == "orangered") {
+      target.parentNode.querySelector(".important-button p").style.color =
+        "white";
+      target.parentNode.style.backgroundColor = "#7A4495";
+      target.parentNode.querySelector(".task").style.color = "#fff";
+      let sItemsForDel = JSON.parse(
+        sessionStorage.getItem(`${currentDiv}-items`)
+      );
+      const index = sItemsForDel.indexOf(id);
+      if (index > -1) {
+        sItemsForDel.splice(index, 1);
+      }
+      sessionStorage.removeItem(`${currentDiv}-items`);
+      sessionStorage.setItem(`${currentDiv}-items`, JSON.stringify(sItemsForDel));
+    } else {
+      target.parentNode.querySelector(".important-button p").style.color =
+        "orangered";
+      target.parentNode.style.backgroundColor = "#ffde20";
+      target.parentNode.querySelector(".task").style.color = "#000";
+      let sessionItems = JSON.parse(sessionStorage.getItem(`${currentDiv}-items`));
+      sessionItems.push(id);
+      sessionStorage.removeItem(`${currentDiv}-items`);
+      sessionStorage.setItem(`${currentDiv}-items`, JSON.stringify(sessionItems));
+    }
+  }
+}
+
+
 const tasks = document.querySelector(".items");
-tasks.addEventListener("click", taskButtons = (event) => {
-    const { target } = event;
+tasks.addEventListener("click", allButtons);
 
-    if (target.classList.contains("tick-icon")) {
-        const task =  target.parentNode.querySelector(".task");
-
-        if(target.parentNode.style.backgroundColor == "green"){
-          task.style.textDecoration = "none";
-          target.parentNode.style.backgroundColor = "#7A4495";
-          target.parentNode.querySelector(".edit-icon").disabled = false;
-          target.parentNode.querySelector(".important-button").disabled = false;
-        }
-        else {
-          task.style.textDecoration = "line-through";
-          target.parentNode.style.backgroundColor = "green";
-          target.parentNode.querySelector(".edit-icon").disabled = true;
-          target.parentNode.querySelector(".important-button").disabled = true;
-        }
-    }
-
-    if(target.classList.contains("delete-icon")){
-      if (confirm("are you sure you want to delete this task?")) {
-        target.parentNode.remove();
-      }
-      else {
-        return;
-      }
-    }
-
-    if(target.classList.contains("edit-icon")){
-      const pText = target.parentNode.querySelector(".task").innerHTML;
-      let textInput = prompt("Please enter your task name:", pText);
-      if (textInput == null || textInput == "") {
-        return;
-      } else {
-        target.parentNode.querySelector(".task").innerHTML = textInput;
-      }
-    }
-
-    if (target.classList.contains("important-button")) {
-      const id = target.parentNode.id;
-      const color = target.parentNode.querySelector(".important-button p").style.color;
-      if (color == "orangered") {
-        target.parentNode
-          .querySelector(".important-button p").style.color = "white";
-          target.parentNode.style.backgroundColor = "#7A4495";
-          target.parentNode.querySelector(".task").style.color = "#fff";
-          let sItemsForDel = JSON.parse(sessionStorage.getItem("main"));
-          const index = sItemsForDel.indexOf(id);
-          if (index > -1) {
-            sItemsForDel.splice(index, 1);
-          }
-          sessionStorage.removeItem("main");
-          sessionStorage.setItem("main", JSON.stringify(sItemsForDel));
-      } else {
-        target.parentNode
-          .querySelector(".important-button p").style.color = "orangered";
-          target.parentNode.style.backgroundColor = "#ffde20";
-          target.parentNode.querySelector(".task").style.color = "#000";
-          let sessionItems = JSON.parse(sessionStorage.getItem('main'));
-          sessionItems.push(id);
-          sessionStorage.removeItem("main");
-          sessionStorage.setItem("main", JSON.stringify(sessionItems));
-      }
-    }
-});
 
 //something cool finished :D now lets make those nav items work!
 
 function justImportant(){
   const items = document.querySelectorAll(".todo-item");
   const id = document.querySelectorAll(".items [id]:not(#create)");
-  let sItems = JSON.parse(sessionStorage.getItem("main"));
+  const sesCur = sessionStorage.getItem("current div");
+  let sItems = JSON.parse(sessionStorage.getItem(`${sesCur}-items`));
   let i = 0;
   items.forEach((item) => {
     item.style.display = "none";
@@ -177,20 +184,21 @@ function justImportant(){
       }
     })
   }
-  document.querySelector(".create-imp").style.display = "block";
-  document.querySelector(".create").style.display = "none";
+  document.querySelector(`#${sesCur} .create-imp`).style.display = "block";
+  document.querySelector(`#${sesCur} .create`).style.display = "none";
 }
 
 const importantThings = document.querySelector(".importants");
 importantThings.addEventListener("click", justImportant);
 
 function showAll() {
+  const sesCur = sessionStorage.getItem("current div");
   const items = document.querySelectorAll(".todo-item");
   items.forEach((item) => {
     item.style.display = "block";
   });
-  document.querySelector(".create-imp").style.display = "none";
-  document.querySelector(".create").style.display = "block";
+  document.querySelector(`#${sesCur} .create-imp`).style.display = "none";
+  document.querySelector(`#${sesCur} .create`).style.display = "block";
 }
 
 
@@ -201,9 +209,19 @@ dayItems.addEventListener("click", showAll);
 // just some copy pastes you know :D
 
 const createNewImportantTask = () => {
-    const itemCount = document.querySelector(".items").childNodes.length;
-    const main = document.querySelector(".items");
-    const create = document.getElementById("create");
+    const ses = sessionStorage.getItem("current div");
+    const itemCount = document.querySelector(`#${ses} .items`).childNodes
+      .length;
+    const main = document.querySelector(`#${ses} .items`);
+    const create = document.querySelector(`#${ses} #create`);
+
+    const textarea = document.createElement("p");
+    let textInput = prompt("Please enter your task name:", "new task");
+    if (textInput == null || textInput == "") {
+      return;
+    } else {
+      textarea.innerHTML = textInput;
+    }
 
     const div = document.createElement("div");
     div.classList.add("todo-item");
@@ -243,24 +261,121 @@ const createNewImportantTask = () => {
     starButton.appendChild(star);
     div.appendChild(starButton);
 
-    const textarea = document.createElement("p");
     textarea.classList.add("task");
     textarea.style.color = "#000";
-    
-    let textInput = prompt("Please enter your task name:", "new task");
-    if (textInput == null || textInput == "") {
-      textarea.innerHTML = "new task";
-    } else {
-      textarea.innerHTML = textInput;
-    }
     div.appendChild(textarea);
 
     const id = div.getAttribute("id");
-    let sessionItems = JSON.parse(sessionStorage.getItem("main"));
+    const divID = sessionStorage.getItem("current div");
+    let sessionItems = JSON.parse(
+      sessionStorage.getItem(`${divID}-items`)
+    );
     sessionItems.push(id);
-    sessionStorage.removeItem("main");
-    sessionStorage.setItem("main", JSON.stringify(sessionItems));
+    sessionStorage.removeItem(`${divID}-items`);
+    sessionStorage.setItem(`${divID}-items`, JSON.stringify(sessionItems));
 }
 
 const createImp = document.getElementById("create-imp");
 createImp.addEventListener("click", createNewImportantTask);
+
+//and the last paaaaaaart
+//these lists "SHOOOOOOOULD work"
+
+const createList = () => {
+  const listNames = document.querySelectorAll(".list-name:not(.list2 .list-name)");
+  const listName = prompt("enter list name", "untitled list");
+  if(listName == null || listName == ""){
+    return;
+  }
+  else {
+    listNames.forEach((name) => {
+      name.innerHTML = listName;
+    });
+  }
+  const contents = document.querySelectorAll('[id*="content"]');
+  contents.forEach((items) => {
+    items.style.display = "none";
+  });
+
+  const container = document.getElementById("container");
+  const containerL = document.getElementById("container").childNodes.length;
+  const content = document.createElement("div");
+  content.setAttribute("id",`content-${containerL-6}`)
+  container.appendChild(content);
+
+  sessionStorage.setItem("current div", `content-${containerL-6}`);
+
+  const listNameDiv = document.createElement("div");
+  listNameDiv.classList.add("list2");
+  content.appendChild(listNameDiv);
+  const listNameP = document.createElement("p");
+  listNameP.classList.add("list-name");
+  listNameDiv.appendChild(listNameP);
+  const spanP = document.createElement("span");
+  spanP.classList.add("pointer");
+  spanP.innerHTML = listName;
+  listNameP.appendChild(spanP);
+
+  const itemsDiv = document.createElement("div");
+  itemsDiv.classList.add("items");
+  itemsDiv.addEventListener("click", allButtons);
+  content.appendChild(itemsDiv);
+  const createDiv = document.createElement("div");
+  createDiv.setAttribute("id","create");
+  createDiv.classList.add("create");
+  createDiv.addEventListener("click", createNewTask);
+  itemsDiv.appendChild(createDiv);
+  const img = document.createElement("img");
+  img.src = "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/25/000000/external-Green-Plus-miscellaneous-kiranshastry-lineal-color-kiranshastry.png";
+  createDiv.appendChild(img);
+  const createP = document.createElement("p");
+  createP.innerHTML = "Create";
+  createDiv.appendChild(createP);
+  const createImp = document.createElement("div");
+  createImp.classList.add("create-imp");
+  createImp.setAttribute("id", "create-imp");
+  createImp.addEventListener("click", createNewImportantTask);
+  itemsDiv.appendChild(createImp);
+  const img2 = document.createElement("img");
+  img2.src = "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/25/000000/external-Green-Plus-miscellaneous-kiranshastry-lineal-color-kiranshastry.png";
+  createImp.appendChild(img2);
+  const createP2 = document.createElement("p");
+  createP2.innerHTML = "Create important task";
+  createImp.appendChild(createP2);
+
+  const listNav = document.createElement("li");
+  listNav.classList.add("list-item");
+  listNav.setAttribute("id", `item-${containerL-6}`);
+  sessionStorage.setItem(`item-${containerL - 6}`, `content-${containerL - 6}`);
+  sessionStorage.setItem(`content-${containerL - 6}-items`, "[]");
+  listNav.innerHTML = listName;
+  document
+    .querySelector(".lists ul")
+    .insertBefore(listNav, document.querySelector(".lists ul .create-list"));
+}
+
+document.querySelector(".create-list").addEventListener("click", createList);
+
+//and the last one for lists to choose :
+
+function listCheck (event){
+  showAll();
+  const {target} = event;
+  if(target.classList.contains("list-item")){
+    const id = target.id;
+    const thisDiv = sessionStorage.getItem(id);
+    const divisions = document.querySelectorAll('[id*="content"]');
+    divisions.forEach(division => {
+      division.style.display = "none";
+    })
+    document.querySelector("#" + thisDiv).style.display = "block";
+    sessionStorage.setItem("current div", thisDiv);
+    target.parentNode.querySelectorAll("li").forEach(node =>{
+      node.style.border = "none";
+    });
+    target.style.borderLeft = "5px groove rgb(236, 8, 84)";
+  }
+}
+
+const selectLists = document.querySelector(".lists-items");
+selectLists.addEventListener("click", listCheck)
